ccf1737611e02c92ec452fb80279794b7bbe0484
Herzanich Georgii <109143250+Fenix524@users.noreply.github.com>
2024-03-11 20:11:10 +0100

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,11 @@
 import css from './HomePage.module.css'
 
 const HomePage = () => {
-	return <div className={css.HomePage}>Home</div>
+	return (
+		<div className={css.HomePage}>
+			<h1 className={css.title}>Welcome to my App</h1>
+		</div>
+	)
 }
 
 export default HomePage
